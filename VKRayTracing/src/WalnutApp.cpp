da011df1a0c23fb9cc0374f8df9c571653fe57cf
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-04-28 01:27:25 +0700

    Слой приложения для отображения рендера

---
+++ VKRayTracing/src/WalnutApp.cpp
@@ -0,0 +1,78 @@
+#include "Walnut/Application.h"
+#include "Walnut/EntryPoint.h"
+
+#include "Walnut/Image.h"
+#include "Walnut/Timer.h"
+#include "Renderer.h"
+
+
+using namespace Walnut;
+
+class ExampleLayer : public Walnut::Layer
+{
+public:
+	virtual void OnUIRender() override
+	{
+		ImGui::Begin("Settings");
+		ImGui::Text("Last render: %.3fms", m_LastRenderTime);
+		if (ImGui::Button("Render"))
+		{
+			Render();
+		}
+		ImGui::End();
+
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("Viewport");
+
+		m_viewPortWidth = ImGui::GetContentRegionAvail().x;
+		m_viewPortHeight = ImGui::GetContentRegionAvail().y;
+
+		auto image = m_Renderer.GetFinalImage();
+		if (image)
+			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth(), (float)image->GetHeight() },
+				{ ImVec2(0, 1) }, { ImVec2(1, 0 ) }
+			);
+
+		ImGui::End();
+		ImGui::PopStyleVar();
+
+		Render();
+	}
+
+	void Render()
+	{
+		Timer timer;
+
+		m_Renderer.onResize(m_viewPortWidth, m_viewPortHeight);
+		m_Renderer.Render();
+
+		m_LastRenderTime = timer.ElapsedMillis();
+	}
+
+private:
+	Renderer m_Renderer;
+	uint32_t m_viewPortWidth = 0, m_viewPortHeight = 0;
+
+	float m_LastRenderTime = 0;
+};
+
+Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
+{
+	Walnut::ApplicationSpecification spec;
+	spec.Name = "Ray Tracing";
+
+	Walnut::Application* app = new Walnut::Application(spec);
+	app->PushLayer<ExampleLayer>();
+	//app->SetMenubarCallback([app]()
+	//	{
+	//		if (ImGui::BeginMenu("File"))
+	//		{
+	//			if (ImGui::MenuItem("Exit"))
+	//			{
+	//				app->Close();
+	//			}
+	//			ImGui::EndMenu();
+	//		}
+	//	});
+	return app;
+}
